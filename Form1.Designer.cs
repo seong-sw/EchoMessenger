1351a220bc6b51081751b482c00a0dffe0ef64a2
seong-sw <seong3533@suwon.ac.kr>
2026-03-19 12:59:30 +0900

    과제1 코딩과 테스트 완료
    Label을 상단에 배치 후 ListBox를 중앙에 크게 배치하고, textBox와 Button을 나란히 배치함
    전송 버튼을 누르면 txtList에 txtMessage에 쓴 문자열이 출력되고, txtMessage에 있는 문자열이 초기화됨

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lbl = new Label();
+            txtList = new ListBox();
+            txtMessage = new TextBox();
+            btnSend = new Button();
+            SuspendLayout();
+            // 
+            // lbl
+            // 
+            lbl.AutoSize = true;
+            lbl.Font = new Font("Pretendard JP Variable", 36F, FontStyle.Bold, GraphicsUnit.Point, 128);
+            lbl.Location = new Point(12, 19);
+            lbl.Name = "lbl";
+            lbl.Size = new Size(576, 86);
+            lbl.TabIndex = 0;
+            lbl.Text = "Echo Messenger";
+            // 
+            // txtList
+            // 
+            txtList.BorderStyle = BorderStyle.FixedSingle;
+            txtList.Font = new Font("Pretendard JP Variable", 11F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            txtList.FormattingEnabled = true;
+            txtList.Location = new Point(32, 126);
+            txtList.Name = "txtList";
+            txtList.Size = new Size(861, 314);
+            txtList.TabIndex = 1;
+            // 
+            // txtMessage
+            // 
+            txtMessage.BorderStyle = BorderStyle.FixedSingle;
+            txtMessage.Font = new Font("Pretendard JP Variable", 11F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            txtMessage.Location = new Point(32, 473);
+            txtMessage.Name = "txtMessage";
+            txtMessage.PlaceholderText = "여기에 텍스트 입력...";
+            txtMessage.Size = new Size(675, 34);
+            txtMessage.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            btnSend.Font = new Font("Pretendard JP Variable Medium", 9F, FontStyle.Bold, GraphicsUnit.Point, 128);
+            btnSend.Location = new Point(735, 473);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(158, 34);
+            btnSend.TabIndex = 3;
+            btnSend.Text = "전송";
+            btnSend.UseVisualStyleBackColor = true;
+            btnSend.Click += btnSend_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(929, 538);
+            Controls.Add(btnSend);
+            Controls.Add(txtMessage);
+            Controls.Add(txtList);
+            Controls.Add(lbl);
+            Name = "Form1";
+            Text = "Echo Messenger";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbl;
+        private ListBox txtList;
+        private TextBox txtMessage;
+        private Button btnSend;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,23 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // 변수 선언 및 txtMessage.Text 값을 할당하여 msg_send 변수에 저장
+            string msg_send;
+            msg_send = txtMessage.Text;
+
+            // txtList에 msg_send 변수의 값을 추가
+            txtList.Items.Add(msg_send);
+
+            // txtMessage.Text 값을 초기화
+            txtMessage.Clear();
+        }
     }
 }
